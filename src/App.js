10f14fce4595c51f4a927c77bb94b6154529232b
idooru <shere1765@gmail.com>
2022-11-29 17:52:07 +0900

    할 일 목록  UI 만들기 (JSX, CSS 작성)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React, { Component } from "react";
-import "./App.css";
+import AppStyle from "./App.module.css";
 
 export default class App extends Component {
-  render() {
+  getStyle = () => {
+    return {
+      padding: "10px",
+      borderBottom: "1px #ccc dotted",
+      textDecoration: "none",
+    };
+  };
+
+  render = () => {
     return (
-      <div className="container">
-        <div className="todoBlock">
+      <div className={AppStyle.container}>
+        <div className={AppStyle.todoBlock}>
           <div className="title">
             <h1>할 일 목록</h1>
+          </div>
+
+          <div style={this.getStyle()}>
+            <input type="checkbox" defaultChecked={false} />
+            공부하기
+            <button className={AppStyle.btnStyle}>x</button>
           </div>
         </div>
       </div>
     );
-  }
+  };
 }
