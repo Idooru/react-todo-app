bdb1e5817cb32295e12b1d1077146028a4dcb2f1
idooru <shere1765@gmail.com>
2022-12-02 15:38:18 +0900

    To-Do 앱을 클래스 컴포넌트에서 함수형 컴포넌트로 바꾸기

--- src/App.js
+++ src/App.js
@@ -1,103 +1,101 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import AppStyle from "./App.module.css";
 
-export default class App extends Component {
-  state = {
-    todoData: [],
-    value: "",
+export default function App() {
+  const [todoData, setTodoData] = useState([]);
+  const [value, setValue] = useState("");
+
+  const handleClickToRemove = (id) => {
+    const newTodoData = todoData.filter((data) => data.id !== id);
+    setTodoData(newTodoData);
   };
 
-  handleClick = (id) => {
-    let newTodoData = this.state.todoData.filter((data) => data.id !== id);
-    super.setState((cur) => (cur.todoData = newTodoData));
+  const handleChange = (event) => {
+    setValue(event.target.value);
   };
 
-  handleChange = (event) => {
-    super.setState((cur) => (cur.value = event.target.value));
-  };
-
-  handleSubmit = (event) => {
-    if (!this.state.value) {
+  const handleSubmit = (event) => {
+    if (!value) {
       return alert("최소 한글자 이상 입력해주세요!");
+    }
+
+    if (value.length > 16) {
+      return alert("16글자를 넘지 말아주세요!");
     }
 
     event.preventDefault();
 
     const newTodo = {
       id: Date.now(),
-      title: this.state.value,
+      title: value,
       completed: false,
     };
 
-    super.setState({
-      todoData: [...this.state.todoData, newTodo],
-      value: "",
-    });
+    setTodoData([...todoData, newTodo]);
+    setValue("");
   };
 
-  handleCompleteChange = (id) => {
-    const newTodoData = this.state.todoData.map((data) => {
+  const handleCompleteChange = (id) => {
+    const newTodoData = todoData.map((data) => {
       if (data.id === id) {
         data.completed = !data.completed;
       }
       return data;
     });
 
-    super.setState({ todoData: newTodoData });
+    setTodoData(() => newTodoData);
   };
 
-  render = () => {
-    return (
-      <div className={AppStyle.container}>
-        <div className={AppStyle.todoBlock}>
-          <div className="title">
-            <h1>할 일 목록</h1>
-          </div>
-
-          {this.state.todoData.map((data) => {
-            const completed = data.completed;
-            const dataId = data.id;
-
-            return (
-              <div
-                className={completed ? AppStyle.todoDone : AppStyle.todoUndone}
-                key={dataId}
-              >
-                <input
-                  type="checkbox"
-                  onChange={() => this.handleCompleteChange(dataId)}
-                  defaultChecked={completed}
-                />
-                {data.title}
-                <button
-                  className={AppStyle.btnStyle}
-                  onClick={() => this.handleClick(dataId)}
-                >
-                  X
-                </button>
-              </div>
-            );
-          })}
-
-          <form className={AppStyle.inputField} onSubmit={this.handleSubmit}>
-            <input
-              onChange={this.handleChange}
-              type="text"
-              name="value"
-              style={{ flex: "10", padding: "5px" }}
-              placeholder="해야 할 일을 입력하세요."
-              autoComplete="off"
-              value={this.state.value}
-            />
-            <input
-              type="submit"
-              value="입력"
-              className="btn"
-              style={{ flex: 1 }}
-            />
-          </form>
+  return (
+    <div className={AppStyle.container}>
+      <div className={AppStyle.todoBlock}>
+        <div className="title">
+          <h1>할 일 목록</h1>
         </div>
+
+        {todoData.map((data) => {
+          const completed = data.completed;
+          const dataId = data.id;
+
+          return (
+            <div
+              className={completed ? AppStyle.todoDone : AppStyle.todoUndone}
+              key={dataId}
+            >
+              <input
+                type="checkbox"
+                onChange={() => handleCompleteChange(dataId)}
+                defaultChecked={completed}
+              />
+              {data.title}
+              <button
+                className={AppStyle.removeBtn}
+                onClick={() => handleClickToRemove(dataId)}
+              >
+                X
+              </button>
+            </div>
+          );
+        })}
+
+        <form className={AppStyle.inputForm} onSubmit={handleSubmit}>
+          <input
+            className={AppStyle.inputArea}
+            onChange={handleChange}
+            type="text"
+            name="value"
+            placeholder="해야 할 일을 입력하세요."
+            autoComplete="off"
+            value={value}
+          />
+          <input
+            className={AppStyle.inputBtn}
+            type="submit"
+            value="입력"
+            style={{ flex: 1 }}
+          />
+        </form>
       </div>
-    );
-  };
+    </div>
+  );
 }
