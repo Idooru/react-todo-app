4b2dfc61f795d0dd849f68798f3cb6b8d258d13b
idooru <shere1765@gmail.com>
2023-01-02 15:34:38 +0900

    List 컴포넌트 생성하기

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./App.module.css";
-import List from "./components/ListComponent/List";
+import Lists from "./components/ListsComponent/Lists";
 import Form from "./components/FormComponent/Form";
 
 export default function App() {
@@ -30,7 +30,7 @@ export default function App() {
         <div className="flex justify-between mb-3">
           <h1>할 일 목록</h1>
         </div>
-        <List todoData={todoData} setTodoData={setTodoData} />
+        <Lists todoData={todoData} setTodoData={setTodoData} />
         <Form
           handleSubmit={handleSubmit}
           setTodoData={setTodoData}
---
+++ src/components/ListsComponent/Lists.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import List from "../ListComponent/List";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+
+export default function Lists({ todoData, setTodoData }) {
+  const handleEnd = (result) => {
+    console.log("result", result);
+
+    if (!result.destination) return;
+    const newTodoData = todoData;
+
+    // 1. 변경시키는 아이템을 배열에서 지워줍니다.
+    // 2. return 값으로 지워진 아이템을 잡아줍니다.
+    const [reorderedItem] = newTodoData.splice(result.source.index, 1);
+
+    // 원하는 자리에 reorderItem을 insert 해줍니다.
+    newTodoData.splice(result.destination.index, 0, reorderedItem);
+    setTodoData(newTodoData);
+  };
+
+  return (
+    <div>
+      <DragDropContext onDragEnd={handleEnd}>
+        <Droppable droppableId="todo">
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              {todoData.map((data, index) => {
+                return (
+                  <Draggable
+                    key={data.id}
+                    draggableId={data.id.toString()}
+                    index={index}
+                  >
+                    {(provided, snapshot) => (
+                      <List
+                        data={data}
+                        provided={provided}
+                        snapshot={snapshot}
+                        todoData={todoData}
+                        setTodoData={setTodoData}
+                      />
+                    )}
+                  </Draggable>
+                );
+              })}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    </div>
+  );
+}
