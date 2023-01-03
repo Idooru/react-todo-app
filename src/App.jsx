854504f24bea413151bba2702f313080e75eec79
idooru <shere1765@gmail.com>
2023-01-03 16:07:38 +0900

    App 컴포넌트와 Box 컴포넌트 분리

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.module.css";
 
 export default function App() {
   return (
-    <div>
+    <div className="flex items-center justify-center w-screen h-screen bg-blue-200">
       <Box />
     </div>
   );
